c33aab23ae823d533f5001b4bd94c5e52ff8992b
Felipe Marinho <felipegigamail@gmail.com>
2024-06-30 17:02:07 -0300

    feat: added filters bar to catalog page

---
+++ app/produtobase/catalogo/filters.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { ChangeEvent, useEffect, useState } from "react";
+
+export default function CatalogFilters() {
+	const router = useRouter();
+	const [parametro, setParametro] = useState("");
+	const [valor, setvalor] = useState("");
+	const [ordenarpor, setOrdenarpor] = useState("sku");
+	const [ordem, setOrdem] = useState("asc");
+
+	const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const _parametro = e.currentTarget.selectedOptions[0].value;
+		setParametro(_parametro);
+	};
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const _valor = e.currentTarget.value;
+		setvalor(_valor);
+	};
+
+	const handleOrdemChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const _ordem = e.currentTarget.selectedOptions[0].value;
+		setOrdem(_ordem);
+	};
+
+	const handleOrderByChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const _ordenarpor = e.currentTarget.selectedOptions[0].value;
+		setOrdenarpor(_ordenarpor);
+	};
+
+	const search = () => {
+		router.push(`?${parametro}=${valor}&ordenarpor=${ordenarpor}&ordem=${ordem}`);
+	};
+
+	useEffect(() => {
+		router.push(`?${parametro}=${valor}&ordenarpor=${ordenarpor}&ordem=${ordem}`);
+	}, [parametro, valor, ordenarpor, ordem]);
+
+	return (
+		<div className="flex items-center justify-between p-4 mb-2 bg-white rounded">
+			<div className="flex gap-2 w-full">
+				<Search size={20} />
+				<span>Buscar por:</span>
+				<select onChange={handleSelectChange} className="border-2 border-gray-400 rounded">
+					<option value="nome" selected={parametro == "nome"}>
+						Nome
+					</option>
+					<option value="estilo" selected={parametro == "estilo"}>
+						Estilo
+					</option>
+					<option value="tamanho" selected={parametro == "tamanho"}>
+						Tamanho
+					</option>
+					<option value="cor" selected={parametro == "cor"}>
+						Cor
+					</option>
+					<option value="sku" selected={parametro == "sku"}>
+						SKU
+					</option>
+				</select>
+
+				<input type="text" value={valor} onChange={handleInputChange} className="border-2 border-gray-400 w-1/2 rounded" />
+			</div>
+			<div className="flex gap-2 flex-shrink-0">
+				<span>Ordenar por:</span>
+				<select onChange={handleOrderByChange} className="border-2 border-gray-400 rounded">
+					<option value="nome" selected={ordenarpor == "nome"}>
+						Nome
+					</option>
+					<option value="estilo" selected={ordenarpor == "estilo"}>
+						Estilo
+					</option>
+					<option value="tamanho" selected={ordenarpor == "tamanho"}>
+						Tamanho
+					</option>
+					<option value="cor" selected={ordenarpor == "cor"}>
+						Cor
+					</option>
+					<option value="sku" selected={ordenarpor == "sku"}>
+						SKU
+					</option>
+				</select>
+				<select onChange={handleOrdemChange} className="border-2 border-gray-400 rounded">
+					<option value="asc" selected={ordem == "asc"}>
+						Crescente
+					</option>
+					<option value="desc" selected={ordem == "desc"}>
+						Decrescente
+					</option>
+				</select>
+			</div>
+		</div>
+	);
+}
--- app/produtobase/catalogo/page.tsx
+++ app/produtobase/catalogo/page.tsx
@@ -2,25 +2,33 @@ import prisma from "@/app/client";
 import CorCircle from "@/app/components/cor_circle";
 import AvaliabilityChecker from "./avaliability";
 import { Suspense } from "react";
+import CatalogFilters from "./filters";
+import { Prisma } from "@prisma/client";
 
 const Loading = () => <div>Verificando...</div>;
 
 export default async function ProdutoBaseSelector({ params, searchParams }: { params: { slug: string }; searchParams: { [key: string]: string | string[] | undefined } }) {
-	const { nome, cor, tamanho, estilo, sku } = searchParams;
+	const { nome, cor, tamanho, estilo, sku, ordenarpor, ordem } = searchParams;
+
+	const orderBy: Prisma.CatalogoOrderByWithRelationAndSearchRelevanceInput = {
+		...(ordenarpor ? { [ordenarpor as string]: ordem } : {}),
+	};
 
 	const _produto = await prisma.catalogo.findMany({
 		where: {
 			nome: { contains: nome as string, mode: "insensitive" },
 			cor: { contains: cor as string, mode: "insensitive" },
-			tamanho: { equals: tamanho as string },
+			tamanho: { contains: tamanho as string, mode: "insensitive" },
 			estilo: { contains: estilo as string, mode: "insensitive" },
-			sku: { equals: sku as string },
+			sku: { contains: sku as string, mode: "insensitive" },
 		},
+		orderBy,
 	});
 
 	return (
 		<div className="flex flex-col h-full">
-			<div className="flex justify-between items-center gap-2 bg-black text-white mb-2 p-2">
+			<CatalogFilters />
+			<div className="flex justify-between items-center gap-2 bg-black text-white mb-2 p-2 rounded">
 				<span className="w-1/6">Nome </span>
 				<span className="w-1/6">Estilo </span>
 				<span className="w-1/6">Tamanho </span>
@@ -29,7 +37,7 @@ export default async function ProdutoBaseSelector({ params, searchParams }: { pa
 				<span className="w-1/6">Disponibilidade</span>
 			</div>
 			{_produto.map((produto) => (
-				<div key={produto.id} className="flex justify-between items-center bg-gray-200 mb-2 p-2">
+				<div key={produto.id} className="flex justify-between items-center bg-gray-200 mb-2 p-2 rounded">
 					<span className="w-1/6 flex items-center gap-2">
 						<CorCircle nome={produto.cor} />
 						{produto.nome}{" "}
